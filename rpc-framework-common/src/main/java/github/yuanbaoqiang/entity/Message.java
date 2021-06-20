cf9267ee6aff01e36f5e29d1ca95a45e0cd2ecfa
YuanbaoQiang <ybq0829@163.com>
2021-06-20 22:52:14 +0800

    socket测试传输实体类

--- rpc-framework-common/src/main/java/github/yuanbaoqiang/entity/Message.java
+++ rpc-framework-common/src/main/java/github/yuanbaoqiang/entity/Message.java
@@ -1,13 +1,12 @@
 package github.yuanbaoqiang.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-import java.io.Serializable;
-
-@AllArgsConstructor
-@Data
-public class Message implements Serializable {
+/**
+ * @program: yuanbaoqiang-toy-rpc
+ * @description: socket传输实体类
+ * @author: YuanbaoQiang
+ * @create: 2021-06-20 22:51
+ */
+public class Message {
     private static final long serialVersionUID = 66234327736493L;
     private String content;
 }
