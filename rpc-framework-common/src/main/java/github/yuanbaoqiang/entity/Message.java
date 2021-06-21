8d530f681a6788222be135958c99d48263327714
YuanbaoQiang <ybqiang@trip.com>
2021-06-21 09:31:08 +0800

    补充lombok注解

--- rpc-framework-common/src/main/java/github/yuanbaoqiang/entity/Message.java
+++ rpc-framework-common/src/main/java/github/yuanbaoqiang/entity/Message.java
@@ -1,11 +1,16 @@
 package github.yuanbaoqiang.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * @program: yuanbaoqiang-toy-rpc
  * @description: socket传输实体类
  * @author: YuanbaoQiang
  * @create: 2021-06-20 22:51
  */
+@Data
+@AllArgsConstructor
 public class Message {
     private static final long serialVersionUID = 66234327736493L;
     private String content;
