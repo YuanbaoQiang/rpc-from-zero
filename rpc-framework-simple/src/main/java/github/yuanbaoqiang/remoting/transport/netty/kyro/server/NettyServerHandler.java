809a72083b5a010d216592f5970bb6e38f3bd872
YuanbaoQiang-ctrip <ybqiang@trip.com>
2021-06-21 15:29:10 +0800

    基于netty

---
+++ rpc-framework-simple/src/main/java/github/yuanbaoqiang/remoting/transport/netty/kyro/server/NettyServerHandler.java
@@ -0,0 +1,43 @@
+package github.yuanbaoqiang.remoting.transport.netty.kyro.server;
+
+import github.yuanbaoqiang.entity.RpcRequest;
+import github.yuanbaoqiang.entity.RpcResponse;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @description: 自定义ChannelHandler处理客户端消息
+ * @author: YuanbaoQiang-ctrip
+ * @create: 2021-06-21 14:08
+ */
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(NettyServerHandler.class);
+    private static AtomicInteger atomicInteger = new AtomicInteger();
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        try {
+            RpcRequest rpcRequest = (RpcRequest) msg;
+            logger.info("server receive msg: [{}] ,times:[{}]", rpcRequest, atomicInteger.getAndIncrement());
+            RpcResponse messageFromServer = RpcResponse.builder().message("message from server").build();
+            ChannelFuture f = ctx.writeAndFlush(messageFromServer);
+            f.addListener(ChannelFutureListener.CLOSE);
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        logger.error("server caught exception", cause);
+        ctx.close();
+    }
+}
