49ed91eaf91bd866a3a6647d1261465e20ca9a60
YuanbaoQiang-ctrip <ybqiang@trip.com>
2021-06-21 09:46:16 +0800

    修改第二台pc的git name，提交测试类

---
+++ rpc-framework-simple/src/main/java/github/yuanbaoqiang/remoting/socket/TestFromOtherPC.java
@@ -0,0 +1,9 @@
+package github.yuanbaoqiang.remoting.socket;
+
+/**
+ * @description: 这是另外一台电脑的git测试类
+ * @author: YuanbaoQiang-ctrip
+ * @create: 2021-06-21 09:44
+ */
+public class TestFromOtherPC {
+}
