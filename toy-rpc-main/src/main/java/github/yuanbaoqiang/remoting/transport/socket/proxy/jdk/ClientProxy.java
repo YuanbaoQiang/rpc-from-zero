665656c8e9524934b1d0ded045a5995f865c19ac
YuanbaoQiang <ybq0829@163.com>
2021-06-24 22:30:04 +0800

    动态代理实现请求和响应的封装，接口的动态查找

--- toy-rpc-main/src/main/java/github/yuanbaoqiang/remoting/transport/socket/proxy/jdk/ClientProxy.java
+++ toy-rpc-main/src/main/java/github/yuanbaoqiang/remoting/transport/socket/proxy/jdk/ClientProxy.java
@@ -3,7 +3,10 @@ package github.yuanbaoqiang.remoting.transport.socket.proxy.jdk;
 import github.yuanbaoqiang.dto.RpcRequest;
 import github.yuanbaoqiang.dto.RpcResponse;
 import github.yuanbaoqiang.remoting.transport.socket.client.RpcSocketClient;
+import github.yuanbaoqiang.service.impl.BlogServiceImpl;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
@@ -17,6 +20,7 @@ import java.lang.reflect.Proxy;
  */
 @AllArgsConstructor
 public class ClientProxy implements InvocationHandler {
+    private static final Logger logger = LoggerFactory.getLogger(BlogServiceImpl.class);
     private String host;
     private int port;
 
@@ -25,18 +29,21 @@ public class ClientProxy implements InvocationHandler {
         // 方法调用之前先对RpcRequest进行封装
         // 比如调用userService.queryUserById(int id)，此时的方法method就是queryUserById
         // getDeclaringClass()返回 方法的类的Class对象，getName()找到具体的Service名字
+        logger.info("开始执行代理类的增强方法，封装rpcRequest");
         RpcRequest rpcRequest = RpcRequest.builder()
                 .interfaceName(method.getDeclaringClass().getName())
                 .methodName(method.getName())
                 .paramTypes(method.getParameterTypes())
                 .args(args).build();
 
+        logger.info("客户端发送rpcRequest");
         RpcResponse rpcResponse = RpcSocketClient.sendMessage(host, port, rpcRequest);
 
         return rpcResponse.getData();
     }
 
     public <T> T getProxyInstance(Class<T> clazz){
+        logger.info("创建代理对象");
         Object o = Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, this);
         return (T) o;
     }
---
+++ toy-rpc-service/src/main/java/github/yuanbaoqiang/service/impl/BlogServiceImpl.java
@@ -0,0 +1,22 @@
+package github.yuanbaoqiang.service.impl;
+
+import github.yuanbaoqiang.dto.Blog;
+import github.yuanbaoqiang.service.BlogService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @description: 新的服务接口的实现类
+ * @author: YuanbaoQiang
+ * @create: 2021-06-23 21:18
+ */
+public class BlogServiceImpl implements BlogService {
+    private static final Logger logger = LoggerFactory.getLogger(BlogServiceImpl.class);
+    @Override
+    public Blog getBlogById(Integer id) {
+        Blog blog = Blog.builder().id(id).title("我的博客").userId(20).build();
+        logger.info("BlogService查询到了id为{}的博客信息", id);
+        return blog;
+    }
+}
